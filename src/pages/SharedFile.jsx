4e565661ccf5c2e648771e3e83d468690a11b66c
Roshan-Rajurkar <roshangit50@gmail.com>
2025-12-07 18:44:56 +0530

    test

--- src/pages/SharedFile.jsx
+++ src/pages/SharedFile.jsx
@@ -4,26 +4,25 @@ import { useAuth } from "../context/AuthContext";
 import { downloadFile } from "../api/files";
 
 const SharedFile = () => {
-  const { id } = useParams();
-  console.log("Got the params", id);
-  const { user } = useAuth();
-  const [message, setMessage] = useState("Preparing your file...");
+  // const { id } = useParams();
+  // const { user } = useAuth();
+  // const [message, setMessage] = useState("Preparing your file...");
 
-  useEffect(() => {
-    const download = async () => {
-      if (!user || !user.token) {
-        setMessage("User not authenticated");
-        return;
-      }
+  // useEffect(() => {
+  //   const download = async () => {
+  //     if (!user || !user.token) {
+  //       setMessage("User not authenticated");
+  //       return;
+  //     }
 
-      const result = await downloadFile(id, user);
-      setMessage(result.message);
-    };
+  //     const result = await downloadFile(id, user);
+  //     setMessage(result.message);
+  //   };
 
-    download();
-  }, [id, user]);
+  //   download();
+  // }, [id, user]);
 
-  return <div>{message}</div>;
+  return <div>Your file is ready</div>;
 };
 
 export default SharedFile;
